137a5674dfa99b4c27818cf4f2cb7fb388e3a326
Ron Knight <ron.knight@gmail.com>
2019-07-31 08:50:29 -0500

    Add files via upload
    Gets a list of missing Windows Updates and outputs to a CSV on the desktop

---
+++ Get-MissingPatches.ps1
@@ -0,0 +1,18 @@
+﻿<#
+Get-MissingPatches
+Provides a list of missing Windows updates and outputs to a CSV file on the desktop
+Ron Knight
+07-31-2019
+#>
+
+Function Get-MissingPatches {
+    get-wmiobject -query "SELECT * FROM CCM_UpdateStatus" -namespace "root\ccm\SoftwareUpdates\UpdatesStore" | where {$_.status -eq "Missing"}
+ }
+
+"MISSING UPDATES:" | Out-file -Filepath "C:\Users\$env:USERNAME\Desktop\$env:Computername`_MissingUpdateInfo_$LogDate.csv" -Append
+if (Get-MissingPatches -gt 0) {
+    Get-MissingPatches | Select-Object Title | Out-file -Filepath "C:\Users\$env:USERNAME\Desktop\$env:Computername`_MissingUpdateInfo_$LogDate.csv" -Append
+    }
+ Else {
+    "NO MISSING UPDATES" | Out-file -Filepath "C:\Users\$env:USERNAME\Desktop\$env:Computername_MissingUpdateInfo_$LogDate.csv" -Append
+    }
